55297580168d234c5205b2be18627ad5daf7c2b9
Simo <Simo Ollinen@ictlab.local>
2019-06-05 14:13:15 +0300

    Updates to UI, added child gameobject to camera

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     private Vector3 targetPosition;
+    public GameObject anchorPoint;
 
     void Start()
     {
@@ -18,11 +19,11 @@ public class Billboard : MonoBehaviour
 
     void UpdatePosition()
     {
-        targetPosition = GazeManager.Instance.HeadPosition + (GazeManager.Instance.GazeDirection * 3.0f);
-        //+ (Camera.main.transform.up * 0.2f) + (Camera.main.transform.right * -0.2f)
+        targetPosition = anchorPoint.transform.position;
+        //GazeManager.Instance.HeadPosition + (GazeManager.Instance.GazeDirection * 3.0f) + (Camera.main.transform.up * 0.2f) + (Camera.main.transform.right * -0.2f)
         float distance = Vector3.Distance(gameObject.transform.position, targetPosition);
 
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, Time.deltaTime * 6.0f);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, 0.09f);
         gameObject.transform.rotation = Camera.main.transform.rotation;
     }
 }
